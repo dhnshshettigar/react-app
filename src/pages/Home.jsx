8951e1092bd4608df87c9226e576e15c842b8595
dhnshshettigar <dhnshshettigar@gmail.com>
2025-06-06 14:24:00 +0530

    Added api.js and made connection to api in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,34 @@
 import MovieCard from "../components/MovieCard"
-import { use, useState } from "react"
+import { useEffect, useState } from "react"
+import { searchMovies, getPolularMovies } from "../services/api"
 import '../css/Home.css'
 
 const Home = () => {
 
     const [searchQuery, setSearchQuery] = useState("");
 
-    const movies = [
-        { id: 1, title: "John Wick", release_date: "2020" },
-        { id: 2, title: "Terminator", release_date: "1999" },
-        { id: 3, title: "The matrix", release_date: "1998" },
-    ]
+    const [movies, setMovies] = useState([]);
+
+    const [error, setError] = useState(null)
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPolularMovies()
+                setMovies(popularMovies)
+            } catch (error) {
+                console.log(error)
+                setError("Failed to load movies")
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadPopularMovies()
+    },[])
+
     return (
         <div className="home">
             <form onSubmit={(e) =>{
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "5c6505128cd1f83744945a83f19905f6"
+const API_URL = "https://api.themoviedb.org/3"
+
+export const getPolularMovies = async () => {
+    const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${API_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+}
